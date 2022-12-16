c92c1e8292d8db0712cf4c640f915359f5d09e3c
Nelson <n911luna@gmail.com>
2022-12-16 14:47:06 -0600

    dropdown menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const Home = ()=>{
     return (
         <div>
-            <h2>Home</h2>
-            <img src="http://www.monsterhunterworld.com/images/top/thumb/img_boost.jpg"/>
+            {/* <img src="http://www.monsterhunterworld.com/images/top/thumb/img_boost.jpg" style={{objectFit:"fill"}}/> */}
         </div>
     )
 }
