b1391bf53ec868954a28f81982896520ed5e1590
Nlsn <n911luna@gmail.com>
2022-12-17 19:20:32 -0600

    reformatted all weapons

--- src/components/Weapons/LongSword.jsx
+++ src/components/Weapons/LongSword.jsx
@@ -22,15 +22,21 @@ const LongSword = (props) => {
 
 
     return (
-        <div className="container p-3 my-3 bg-dark text-white">
+        <div className="container-fluid p-3 my-3  text-white">
             <div className="row">
                 {lsData.length ? (lsData.map((weapon,index) => {
                     return (
-                        <div className="col" key={`weapon-${index}`}>
-                        <img src={filter(weapon.assets)?weapon.assets.image:"N/A"}/>
-                        <img src={filter(weapon.assets)?weapon.assets.icon:"N/A"}/>
+                        <div className="col-md-3 mt-3" id="weaponCard" key={`weapon-${index}`}>
+                        <div className="weapon-container-outer" style={{width:"600px", backgroundColor:'black'}}>
+                         <div className="weapon-info" style={{border: "2px solid green"}}>    
                         <h2>{weapon.name}</h2>
+                        <img  style={{maxWidth: "20px", maxHeight: "20px"}} src={filter(weapon.assets)?weapon.assets.icon:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fdigitalcommons.georgiasouthern.edu%2Fcontext%2Fjesuit-gallery205%2Farticle%2F1000%2Ftype%2Fnative%2Fviewcontent&imgrefurl=https%3A%2F%2Fdigitalcommons.georgiasouthern.edu%2Fjesuit-gallery205%2F1%2F&tbnid=_UrqdYQ9FIqGiM&vet=12ahUKEwignuyI8oH8AhXtKt4AHYIfD1kQMygFegUIARDzAQ..i&docid=REtbhB_SYuRj-M&w=428&h=428&q=image%20coming%20soon&ved=2ahUKEwignuyI8oH8AhXtKt4AHYIfD1kQMygFegUIARDzAQ"}/>         
+                        </div>
 
+                        <div style={{border: "2px solid red"}}>
+                        <img style={{maxWidth: "100px", maxHeight: "100px"}} src={filter(weapon.assets)?weapon.assets.image:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fdigitalcommons.georgiasouthern.edu%2Fcontext%2Fjesuit-gallery205%2Farticle%2F1000%2Ftype%2Fnative%2Fviewcontent&imgrefurl=https%3A%2F%2Fdigitalcommons.georgiasouthern.edu%2Fjesuit-gallery205%2F1%2F&tbnid=_UrqdYQ9FIqGiM&vet=12ahUKEwignuyI8oH8AhXtKt4AHYIfD1kQMygFegUIARDzAQ..i&docid=REtbhB_SYuRj-M&w=428&h=428&q=image%20coming%20soon&ved=2ahUKEwignuyI8oH8AhXtKt4AHYIfD1kQMygFegUIARDzAQ"}/>
+                        </div>
+                        </div>
                         </div>
                     )
                 })): <>Loading LongSword...</>}
