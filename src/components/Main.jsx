5e612704084a5c3a29fd2988ce88524c398de8af
Nlsn <n911luna@gmail.com>
2022-12-20 16:34:14 -0600

    can now get item by id for full data features, gotta implement the latter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,9 +3,12 @@ import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements,Ro
 import {Ailments,Navbar, Home, Armor} from './'
 import { GetAllWeapons } from "../api";
 import { GreatSword,Bow,ChargeBlade,DualBlades,Gunlance,Hammer,Hbg,HuntingHorn,InsectGlaive,Lance,Lbg,LongSword,SnS,SwitchAxe } from "./Weapons";
+import SingleWeapon from "./Weapons/SingleWeapon";
+import axios from "axios";
 
 const Main = ()=> {
     const [weaponData,setWeaponData] = useState([])
+    const [bowData, setBowData] = useState([])
 
     useEffect(() => {
         const getWeapons = async () => {
@@ -15,6 +18,22 @@ const Main = ()=> {
         }
         getWeapons();
     },[])
+
+    useEffect(() => {
+        axios.get("https://mhw-db.com/weapons", {
+            params: {
+                q: JSON.stringify ({
+                    type: "bow"
+                })
+            }
+        })
+        .then((response) => {
+            console.log(response.data)
+            setBowData(response.data)
+        })
+       
+    },[])
+
 
    
 
@@ -26,7 +45,7 @@ const Main = ()=> {
                 <Route path="/" element={<Home />}/>
                 <Route path="Ailments" element={<Ailments />} />
                 <Route path="Armor" element={<Armor />}/>
-                <Route path="Bow" element={<Bow  weaponData={weaponData}/>} />
+                <Route path="Bow" element={<Bow  bowData={bowData}/>} />
                 <Route path="Charge-Blade" element={<ChargeBlade  weaponData={weaponData}/>} />
                 <Route path="Dual-Blades" element={<DualBlades  weaponData={weaponData}/>} />
                 <Route path="Great-Sword" element={<GreatSword  weaponData={weaponData}/>} />
@@ -40,6 +59,22 @@ const Main = ()=> {
                 <Route path="Long-Sword" element={<LongSword  weaponData={weaponData}/>} />
                 <Route path="Sword-and-Shield" element={<SnS  weaponData={weaponData}/>} />
                 <Route path="Switch-Axe" element={<SwitchAxe  weaponData={weaponData}/>} />
+                <Route path="SingleWeapon" element={<SingleWeapon weaponData={weaponData}/>}/>
+
+                <Route path="Bow/:id" element={<SingleWeapon  weaponData={weaponData}/>} />
+                <Route path="Charge-Blade/:id" element={<SingleWeapon  weaponData={weaponData}/>} />
+                <Route path="Dual-Blades/:id" element={<DualBlades  weaponData={weaponData}/>} />
+                <Route path="Great-Sword/:id" element={<GreatSword  weaponData={weaponData}/>} />
+                <Route path="Gunlance/:id" element={<Gunlance  weaponData={weaponData}/>} />
+                <Route path="Hammer/:id" element={<Hammer  weaponData={weaponData}/>} />
+                <Route path="Heavy-Bow-Gun/:id" element={<Hbg  weaponData={weaponData}/>} />
+                <Route path="Hunting-Horn/:id" element={<HuntingHorn  weaponData={weaponData}/>} />
+                <Route path="Insect-Glaive/:id" element={<InsectGlaive  weaponData={weaponData}/>} />
+                <Route path="Lance/:id" element={<Lance  weaponData={weaponData}/>} />
+                <Route path="Light-Bow-Gun/:id" element={<Lbg  weaponData={weaponData}/>} />
+                <Route path="Long-Sword/:id" element={<LongSword  weaponData={weaponData}/>} />
+                <Route path="Sword-and-Shield/:id" element={<SnS  weaponData={weaponData}/>} />
+                <Route path="Switch-Axe/:id" element={<SwitchAxe  weaponData={weaponData}/>} />
             </Route>
             )
         );
