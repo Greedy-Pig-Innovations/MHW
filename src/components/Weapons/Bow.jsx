31f66c6009c65cb0acbcfe4496d9dd14c04ff121
Juan <jjperez1990.jp@Gmail.com>
2022-12-20 20:53:56 -0600

    update

--- src/components/Weapons/Bow.jsx
+++ src/components/Weapons/Bow.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
@@ -11,22 +12,12 @@ const Bow = (props) => {
     const imgNotFound = "https://res.cloudinary.com/dofkj8qo6/image/upload/v1671577838/Image_Coming_Soon_zl2uti.png";
 
     const handleClick = (id) => {
-<<<<<<< HEAD
-        console.log("clicked item: ", id)
         axios.get(`https://mhw-db.com/weapons/${id}`)
             .then((response) => {
                 console.log(response)
                 setSingleWeapon(response.data)
             })
     }
-=======
-                axios.get(`https://mhw-db.com/weapons/${id}`)
-                .then((response) => {
-                    console.log(response)
-                    setSingleWeapon(response.data)
-                })         
-        }
->>>>>>> master
 
 
 
@@ -39,44 +30,34 @@ const Bow = (props) => {
 
     return (
         <div className="container-fluid row text-light MHW-font">
-            <div className="sideBar col-3 row-fluid px-5">
-                <div className=" bg-evil" style={{ height: "60vh", textAlign: "center" }}>
+            <div className="sideBar col-3 row-lg px-5 mt-5" >
+                <div className="bg-evil" style={{ height: "60vh", textAlign: "center", width: "340px" }}>
                     {singleWeapon && singleWeapon.id ?
                         <div>
-<<<<<<< HEAD
-                            <div className="mt-3" style={{ border: "3px solid rgb(100, 50, 150)" }}>
+                            <div style={{ borderBottom: "1px solid #f8f9fa" }}>
                                 <h2>{singleWeapon.name}</h2>
                             </div>
+                            <div style={{ borderBottom: "1px solid #f8f9fa" }}>
+                                <img src={singleWeapon.assets.image} style={{ width: "200px" }} />
+                            </div>
+                            <div className="row my-2 bb-light">
+                                <div className="col">
+                                    <span>Rarity: {singleWeapon.rarity}</span>
+                                </div>
+                            </div>
 
-                            <img className="" src={singleWeapon.assets.image} />
-
-                            <span>Rarity: {singleWeapon.rarity}</span>
                             <p>Raw: {singleWeapon.attack.raw}</p>
                             <span>Display:{singleWeapon.attack.display}</span>
                             <p>Coatings: {singleWeapon.coatings.length ? singleWeapon.coatings.map((coating, index) => {
                                 return (
                                     <div key={`coating-${index}`}>
-                                        <p>{coating}</p>
+                                        <div>
+                                            <p>{coating}</p>
+                                        </div>
                                     </div>
                                 )
                             }) : <>None</>}</p>
-=======
-                        <h2>{singleWeapon.name}</h2>
-                        <span>Rarity: {singleWeapon.rarity}</span>
-                        </div>
-                        <img src={singleWeapon.assets.image} />
-                        <div>
-                        <p>Raw: {singleWeapon.attack.raw}</p>
-                        <span>Display:{singleWeapon.attack.display}</span>
-                        </div>
-                        <div>Coatings: {singleWeapon.coatings.length ? singleWeapon.coatings.map((coating, index) => {
-                            return (
-                                <div key={`coating-${index}`}>
-                                    <p>{coating}</p>
-                                </div>
-                            )   
-                        }) : <>None</>}</div>
->>>>>>> master
+
                         </div>
                         : <></>}
                 </div>
