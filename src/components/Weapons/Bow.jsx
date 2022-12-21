23b7803af3948fd2c1f86eb98ae56012c30fa97a
Nlsn <n911luna@gmail.com>
2022-12-20 18:19:10 -0600

    single weapon display capability

--- src/components/Weapons/Bow.jsx
+++ src/components/Weapons/Bow.jsx
@@ -5,15 +5,19 @@ import SingleWeapon from "./SingleWeapon";
 
 const Bow = (props) => {
     const bowData = props.bowData
+    const setSingleWeapon = props.setSingleWeapon
+    const singleWeapon = props.singleWeapon
     const imgNotFound = "https://res.cloudinary.com/dofkj8qo6/image/upload/v1671577838/Image_Coming_Soon_zl2uti.png";
 
     const handleClick = (id) => {
         console.log("clicked item: ", id)
-        const singleWeapon = bowData.filter(weapon => {
-            return weapon.id == id
-        })
+                axios.get(`https://mhw-db.com/weapons/${id}`)
+                .then((response) => {
+                    console.log(response)
+                    setSingleWeapon(response.data)
+                })         
+        }
 
-    }
 
     const filter = (assets) => {
         if (assets)
@@ -26,6 +30,25 @@ const Bow = (props) => {
         <div className="container-fluid row text-light MHW-font">
             <div className="sideBar col-3 row-fluid px-5">
                 <div className="card bg-secondary" style={{ height: "60vh" }}>
+                    {singleWeapon && singleWeapon.id ? 
+                        <div>
+                        <div>
+                        <h2>{singleWeapon.name}</h2>
+                        <span>Rarity: {singleWeapon.rarity}</span>
+                        </div>
+                        <img src={singleWeapon.assets.image} />
+
+                        <p>Raw: {singleWeapon.attack.raw}</p>
+                        <span>Display:{singleWeapon.attack.display}</span>
+                        <p>Coatings: {singleWeapon.coatings.length ? singleWeapon.coatings.map((coating, index) => {
+                            return (
+                                <div key={`coating-${index}`}>
+                                    <p>{coating}</p>
+                                </div>
+                            ) 
+                        }) : <>None</>}</p>
+                        </div>
+                 : <></> }
                 </div>
             </div>
             <div className="col">
