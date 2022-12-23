e3e8a59f306927136531dabe2b9d2f18ab7e9ad0
Juan <jjperez1990.jp@Gmail.com>
2022-12-22 18:29:32 -0600

    armor update

--- src/components/Weapons.jsx
+++ src/components/Weapons.jsx
@@ -1,16 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { displayData } from './helperFunctions';
 
 
 const Weapons = (props) => {
-    const [shown,setShown] = useState("great-sword");
-    const [data,setData] = useState([]);
+    const [shown, setShown] = useState("great-sword");
+    const [data, setData] = useState([]);
     const weapon = props.weapon
     const setWeapon = props.setWeapon
 
     useEffect(() => {
-        axios.get('https://mhw-db.com/weapons',{
+        axios.get('https://mhw-db.com/weapons', {
             params: {
                 q: JSON.stringify({
                     type: shown
@@ -20,7 +20,7 @@ const Weapons = (props) => {
             console.log(res.data)
             setData(res.data)
         })
-    },[shown])
+    }, [shown])
 
     const handleClick = (id) => {
         axios.get(`https://mhw-db.com/weapons/${id}`).then((response) => {
@@ -32,52 +32,52 @@ const Weapons = (props) => {
 
     return (
         <div className="row">
-        <span className="col-3 mt-5 ml-5">
-            <div className="bg-blur text-white MHW-font p-2 jp-shadow" style={{borderRadius: "10px"}}>
-                {weapon && weapon.id ?
-                            displayData(weapon.name,weapon.rarity,weapon.assets.image,weapon.slots,weapon.attack.raw,weapon.damageType,weapon.attributes.affinity, weapon.attributes.defense,weapon.durability)  
-                    : <></>}
-            </div>
-        </span>
-        <div className="col-7">
-            <div className="mt-5 p-4 jp-shadow bg-blur text-light MHW-font" style={{ borderRadius: "10px" }}>
-                <ul className="row text-center">
-                    <a className="col nav-link link link--elara" onClick={() => setShown("great-sword")}>Great Sword</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("sword-and-shield")}>Sword and Shield</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("dual-blades")}>Dual Blades</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("long-sword")}>Long Sword</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("hammer")}>Hammer</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("hunting-horn")}>Hunting Horn</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("lance")}>Lance</a>
-                </ul>
-                <ul className="row text-center">
-                    <a className="col nav-link link link--elara" onClick={() => setShown("gunlance")}>Gunlance</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("switch-axe")}>Switch Axe</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("charge-blade")}>Charge Blade</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("insect-glaive")}>Insect Glaive</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("bow")}>Bow</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("light-bowgun")}>Light Bowgun</a>
-                    <a className="col nav-link link link--elara" onClick={() => setShown("heavy-bowgun")}>Heavy Bowgun</a>
-                </ul>
+            <span className="col-3 mt-5 ml-5">
+                <div className="bg-blur text-white MHW-font p-2 jp-shadow" style={{ borderRadius: "10px" }}>
+                    {weapon && weapon.id ?
+                        displayData(weapon.name, weapon.rarity, weapon.assets.image, weapon.slots, weapon.attack.raw, weapon.elements, weapon.damageType, weapon.attributes.affinity, weapon.attributes.defense, weapon.durability)
+                        : <></>}
+                </div>
+            </span>
+            <div className="col-7">
+                <div className="mt-5 p-4 jp-shadow bg-blur text-light MHW-font" style={{ borderRadius: "10px" }}>
+                    <ul className="row text-center">
+                        <a className="col nav-link link link--elara" onClick={() => setShown("great-sword")}>Great Sword</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("sword-and-shield")}>Sword and Shield</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("dual-blades")}>Dual Blades</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("long-sword")}>Long Sword</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("hammer")}>Hammer</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("hunting-horn")}>Hunting Horn</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("lance")}>Lance</a>
+                    </ul>
+                    <ul className="row text-center">
+                        <a className="col nav-link link link--elara" onClick={() => setShown("gunlance")}>Gunlance</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("switch-axe")}>Switch Axe</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("charge-blade")}>Charge Blade</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("insect-glaive")}>Insect Glaive</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("bow")}>Bow</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("light-bowgun")}>Light Bowgun</a>
+                        <a className="col nav-link link link--elara" onClick={() => setShown("heavy-bowgun")}>Heavy Bowgun</a>
+                    </ul>
 
-                <div className="row" id={shown} style={{ borderTop: "2px solid rgba(150,150,150,.5)", height: "70vh", overflow: "auto", overflowX: "hidden", borderBottom: "2px solid rgba(150,150,150,.5)" }}>
-                    {data.length ? data.map((dt, index) => {
+                    <div className="row" id={shown} style={{ borderTop: "2px solid rgba(150,150,150,.5)", height: "70vh", overflow: "auto", overflowX: "hidden", borderBottom: "2px solid rgba(150,150,150,.5)" }}>
+                        {data.length ? data.map((dt, index) => {
 
-                        if (dt.assets) {
-                            return (
-                                <div className="col p-2 m-2 item" key={index} onClick={() => handleClick(dt.id)}>
-                                    <img className="card-img-top" src={dt.assets.image} style={{width: "200px", minHeight: "175px"}} />
-                                    <div className="card-title" style={{ textAlign: "center", borderRadius: "10px", borderBottom: "2px solid rgba(150,150,150,.5)" }}>{dt.name}</div>
-                                </div>
-                            )
-                        }
-                    }) : (<div className="lds-dual-ring"></div>)}
+                            if (dt.assets) {
+                                return (
+                                    <div className="col p-2 m-2 item" key={index} onClick={() => handleClick(dt.id)}>
+                                        <img className="card-img-top" src={dt.assets.image} style={{ width: "200px", minHeight: "175px" }} />
+                                        <div className="card-title" style={{ textAlign: "center", borderRadius: "10px", borderBottom: "2px solid rgba(150,150,150,.5)" }}>{dt.name}</div>
+                                    </div>
+                                )
+                            }
+                        }) : (<div className="lds-dual-ring"></div>)}
+
+                    </div>
 
                 </div>
-
             </div>
         </div>
-    </div>
     )
 }
 
--- src/components/helperFunctions.jsx
+++ src/components/helperFunctions.jsx
@@ -1,252 +1,262 @@
-export const assignElement = (type, damage, index) => {
-  switch (type) {
-    case "blast":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670235/Mhw/Blastblight_mylbi3.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "poison":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670261/Mhw/Poison_uzyxp9.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "dragon":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670287/Mhw/download_mizgia.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "thunder":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670303/Mhw/download_n2sfop.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "ice":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670330/Mhw/download_njoqfy.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "sleep":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670355/Mhw/download_ifvf9n.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "fire":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670543/Mhw/download_q7vvae.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "water":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670557/Mhw/download_ibmjx7.png"
-          />
-          <>{damage}</>
-        </div>
-      );
-      break;
-    case "paralysis":
-      return (
-        <div key={index}>
-          <img
-            className="e-icon"
-            key={index}
-            src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670574/Mhw/download_b61mup.png"
-          />
-          <>{damage}</>
-        </div>
-      );
+export const assignElement = (type, damage, index, hide) => {
 
-      break;
-  }
+    const hidden = (dmg) => {
+        return `(${dmg})`;
+    }
+
+    switch (type) {
+        case "blast":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670235/Mhw/Blastblight_mylbi3.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "poison":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670261/Mhw/Poison_uzyxp9.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "dragon":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670287/Mhw/download_mizgia.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "thunder":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670303/Mhw/download_n2sfop.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "ice":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670330/Mhw/download_njoqfy.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "sleep":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670355/Mhw/download_ifvf9n.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "fire":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670543/Mhw/download_q7vvae.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "water":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670557/Mhw/download_ibmjx7.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+            break;
+        case "paralysis":
+            return (
+                <div key={index}>
+                    <img
+                        className="e-icon"
+                        key={index}
+                        src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671670574/Mhw/download_b61mup.png"
+                    />
+                    <>{hide ? hidden(damage) : damage}</>
+                </div>
+            );
+
+            break;
+    }
 };
 
 export const assignSharpness = (
-  red,
-  orange,
-  yellow,
-  green,
-  blue,
-  white,
-  purple,
-  idx
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    white,
+    purple,
+    idx
 ) => {
-  return (
-    <div className="sharpness-bar" key={`durability-${idx}`}>
-      <div
-        className="red"
-        style={{
-          width: `${Math.floor((red / 400) * 100)}px`,
-        }}
-      ></div>
-      <div
-        className="orange"
-        style={{
-          width: `${Math.floor((orange / 400) * 100)}px`,
-        }}
-      ></div>
-      <div
-        className="yellow"
-        style={{
-          width: `${Math.floor((yellow / 400) * 100)}px`,
-        }}
-      ></div>
-      <div
-        className="green"
-        style={{
-          width: `${Math.floor((green / 400) * 100)}px`,
-        }}
-      ></div>
-      <div
-        className="blue"
-        style={{
-          width: `${Math.floor((blue / 400) * 100)}px`,
-        }}
-      ></div>
-      <div
-        className="white"
-        style={{
-          width: `${Math.floor((white / 400) * 100)}px`,
-        }}
-      ></div>
-      <div
-        className="purple"
-        style={{
-          width: `${Math.floor((purple / 400) * 100)}px`,
-        }}
-      ></div>
-      <div className="clearfix"></div>
-    </div>
-  );
+    return (
+        <div className="sharpness-bar" key={`durability-${idx}`}>
+            <div
+                className="red"
+                style={{
+                    width: `${Math.floor((red / 400) * 100)}px`,
+                }}
+            ></div>
+            <div
+                className="orange"
+                style={{
+                    width: `${Math.floor((orange / 400) * 100)}px`,
+                }}
+            ></div>
+            <div
+                className="yellow"
+                style={{
+                    width: `${Math.floor((yellow / 400) * 100)}px`,
+                }}
+            ></div>
+            <div
+                className="green"
+                style={{
+                    width: `${Math.floor((green / 400) * 100)}px`,
+                }}
+            ></div>
+            <div
+                className="blue"
+                style={{
+                    width: `${Math.floor((blue / 400) * 100)}px`,
+                }}
+            ></div>
+            <div
+                className="white"
+                style={{
+                    width: `${Math.floor((white / 400) * 100)}px`,
+                }}
+            ></div>
+            <div
+                className="purple"
+                style={{
+                    width: `${Math.floor((purple / 400) * 100)}px`,
+                }}
+            ></div>
+            <div className="clearfix"></div>
+        </div>
+    );
 };
 
 export const assignSlots = (slot, idx) => {
-  switch (slot) {
-    case 1:
-      return (
-        <img
-          key={`slot-${idx}`}
-          src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671664648/Mhw/gem_level_1_aaolcn.png"
-        />
-      );
-      break;
-    case 2:
-      return (
-        <img
-          key={`slot-${idx}`}
-          src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671664781/Mhw/gem_level_2_waqkwk.png"
-        />
-      );
-      break;
-    case 3:
-      return (
-        <img
-          key={`slot-${idx}`}
-          src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671664734/Mhw/gem_level_3_zgidct.png"
-        />
-      );
-      break;
-  }
+    switch (slot) {
+        case 1:
+            return (
+                <img
+                    key={`slot-${idx}`}
+                    src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671664648/Mhw/gem_level_1_aaolcn.png"
+                />
+            );
+            break;
+        case 2:
+            return (
+                <img
+                    key={`slot-${idx}`}
+                    src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671664781/Mhw/gem_level_2_waqkwk.png"
+                />
+            );
+            break;
+        case 3:
+            return (
+                <img
+                    key={`slot-${idx}`}
+                    src="https://res.cloudinary.com/daif0s5gi/image/upload/v1671664734/Mhw/gem_level_3_zgidct.png"
+                />
+            );
+            break;
+    }
 };
 
-export const displayData = (name,rare,image,slots,raw,dmgType,affinity,defense,durability,index) => {
-   return  <div key={`weapon-${index}`}>
-      <div>
-        <h2>{name}</h2>
-        <p>R:{rare}</p>
-      </div>
-      <div>
-        <img src={image}/>
-      </div>
-      <div className="slots">
-      Slots:
-      {slots.length ? slots.map((slot,idx) => {
-         return  assignSlots(slot.rank,idx)
-      })  : <></>}
-      </div>
-      <div className="attk-val">
-        <p>Raw: {raw}</p>
-      </div>
-      
+export const displayData = (name, rare, image, slots, raw, elements, dmgType, affinity, defense, durability, index) => {
+    return <div key={`weapon-${index}`}>
+        <div>
+            <h2>{name}</h2>
+            <p>R:{rare}</p>
+        </div>
+        <div>
+            <img src={image} />
+        </div>
+        <div className="slots">
+            Slots:
+            {slots.length ? slots.map((slot, idx) => {
+                return assignSlots(slot.rank, idx)
+            }) : <></>}
+        </div>
+        <div className="attk-val">
+            <p>Raw: {raw}</p>
+            <div>Elements: {elements.length ? elements.map((ele, idx) => {
+                { console.log(ele) }
+                return ele.hidden ? (assignElement(ele.type, ele.damage, idx, true)) : assignElement(ele.type, ele.damage, idx)
+            }) : <>None</>}
+            </div>
+        </div>
 
 
-      <div className="attributes">
-      Attributes:
-        <p> Affinity:
-           {affinity ? (
-              affinity
+
+        <div className="attributes">
+            Attributes:
+            <p> Affinity:
+                {affinity ? (
+                    affinity
                 ) : (
-            <>0</>
-          )} %
-        </p>
-        <p>
-          Defense:
-            {defense ? (
-              defense
+                    <>0</>
+                )} %
+            </p>
+            <p>
+                Defense:
+                {defense ? (
+                    defense
                 ) : (
-                  <>0</>
-            )}
-        </p>
-      </div>
-      {dmgType !== "projectile" ? 
-      <div className="sharpness">
-        Base sharpness, incremented by levels of the Handicraft SKill:
-        {durability.length ? durability.map((guage,idx) => {
-          console.log(guage)
-          return assignSharpness(guage.red,guage.orange,guage.yellow,guage.green,guage.blue,guage.white,guage.purple,idx)
-        }): <></> }
-      </div>
-      : <></>}
+                    <>0</>
+                )}
+            </p>
+        </div>
+        {dmgType !== "projectile" ?
+            <div className="sharpness">
+                Base sharpness, incremented by levels of the Handicraft SKill:
+                {durability.length ? durability.map((guage, idx) => {
+                    console.log(guage)
+                    return assignSharpness(guage.red, guage.orange, guage.yellow, guage.green, guage.blue, guage.white, guage.purple, idx)
+                }) : <></>}
+            </div>
+            : <></>}
 
 
     </div>
